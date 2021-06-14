3ccfa2c0933532675dff83a5d6b59bbb780f8e5b
Jazzer9F <Jazzer9F@users.noreply.github.com>
2021-06-14 14:27:54 +0200

    Code structure & comments added

--- contracts/DeFiPlaza.sol
+++ contracts/DeFiPlaza.sol
@@ -9,44 +9,52 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 /**
  * DeFi Plaza is a single controct, multi token DEX which allows trades between any two tokens.
  * Trades between two tokens always follow the familiar localized bonding curve x*y=k
+ * The number of tokens used is hardcoded to 16 for efficiency reasons.
  */
 contract DeFiPlaza is IDeFiPlaza, Ownable, ERC20 {
   using SafeMath for uint256;
 
+  // States that each token can be in
   enum State {Unlisted, PreListing, Delisting, Listed}
 
+  // Configuration per token. Still some bits available if needed
   struct TokenSettings {
     State state;                      // What state the token is currently in
     uint112 listingTarget;            // Amount of tokens needed to activate listing
   }
 
+  // Exchange configuration
   struct Config {
     bool unlocked;                    // Locked for trading to prevent re-entrancy misery
     uint64 oneMinusTradingFee;        // One minus the swap fee (0.64 fixed point integer)
-    uint40 delistingBonus;            // Amount of additional tokens to encourage people taking
+    uint40 delistingBonus;            // Amount of additional tokens to encourage immediate delisting (0.40 fixed point)
   }
 
+  // Keeps track of whether there is a listing change underway and if so between which tokens
   struct ListingUpdate {
     address tokenToDelist;            // Token to be removed
     address tokenToList;              // Token to be listed
   }
 
+  // Mapping to keep track of the listed tokens
   mapping(address => TokenSettings) public listedTokens;
   Config public DFP_config;
   ListingUpdate public listingUpdate;
 
   /**
-  * Only works correctly when invoked with full list of 16 token addresses.
-  * First address needs to be zero address to represent ETH.
+  * Sets up default configuration
+  * Initialize with ordered list of 15 token addresses (ETH is always listed)
   * Doesn't do any checks. Make sure you ONLY add well behaved ERC20s!!
   */
   constructor(address[] memory tokensToList, string memory name_, string memory symbol_) ERC20(name_, symbol_) {
+    // Basic exchange configureation
     Config memory config;
     config.unlocked = false;
-    config.oneMinusTradingFee = 0xffbe76c8b4395800;
+    config.oneMinusTradingFee = 0xffbe76c8b4395800;   // approximately 0.999
     config.delistingBonus = 0;
     DFP_config = config;
 
+    // Configure the listed tokens as such
     TokenSettings memory listed;
     listed.state = State.Listed;
     require(tokensToList.length == 15, "Incorrect number of tokens");
@@ -59,11 +67,14 @@ contract DeFiPlaza is IDeFiPlaza, Ownable, ERC20 {
       previous = current;
     }
 
+    // Generate the LP tokens reflecting the initial liquidity (to be loaded externally)
     _mint(msg.sender, 1600e18);
   }
 
-  receive() external payable {}  // For bootstrapping ETH liquidity
+  // For bootstrapping ETH liquidity
+  receive() external payable {}
 
+  // To safeguard some functionality is only applied to listed tokens
   modifier onlyListedToken(address token) {
     require(
       token == address(0) || listedTokens[token].state > State.Delisting,
@@ -73,12 +84,9 @@ contract DeFiPlaza is IDeFiPlaza, Ownable, ERC20 {
   }
 
   /**
-      Allows users to swap between any two tokens listed on the DEX.
-      The invariant used for swaps between two tokens is the familiar UniSwap AMM hyperbole:
-
-                      x * y = k
-
-      For ETH trades, send the ETH with the transaction and use the NULL address as inputToken.
+  * Allows users to swap between any two tokens listed on the DEX.
+  * Follows the x*y=k swap invariant hyperbole
+  * For ETH trades, send the ETH with the transaction and use the NULL address as inputToken.
   */
   function swap(
     address inputToken,
@@ -93,9 +101,11 @@ contract DeFiPlaza is IDeFiPlaza, Ownable, ERC20 {
     override
     returns (uint256 outputAmount)
   {
+    // Check that the exchange is unlocked and thus open for business
     Config memory _config = DFP_config;
     require(_config.unlocked, "DFP: Locked");
 
+    // Pull in input token and check the exchange balance for that token
     uint256 initialInputBalance;
     if (inputToken == address(0)) {
       require(msg.value == inputAmount, "DFP: bad ETH amount");
@@ -108,42 +118,41 @@ contract DeFiPlaza is IDeFiPlaza, Ownable, ERC20 {
       );
     }
 
+    // Check dex balance of the output token
     uint256 initialOutputBalance;
     if (outputToken == address(0)) {
       initialOutputBalance = address(this).balance;
     } else {
       initialOutputBalance = IERC20(outputToken).balanceOf(address(this));
     }
+
+    // Calculate the output amount through the x*y=k invariant
     // Can skip overflow/underflow checks on this calculation as they will always work against an attacker anyway.
     uint256 netInputAmount = inputAmount * _config.oneMinusTradingFee;
     outputAmount = netInputAmount * initialOutputBalance / ((initialInputBalance << 64) + netInputAmount);
     require(outputAmount > minOutputAmount, "DFP: No deal");
 
+    // Send output tokens to whoever invoked the swap function
     if (outputToken == address(0)) {
       address payable sender = msg.sender;
       sender.transfer(outputAmount);
     } else {
       IERC20(outputToken).transfer(msg.sender, outputAmount);
     }
-    // emitting events costs gas, but I feel it is needed to allow informed governance decisions
+
+    // Emit swap event to enable better governance decision making
     emit Swapped(msg.sender, inputToken, outputToken, inputAmount, outputAmount);
   }
 
   /**
-      Adding liquidity to the DEX is a single sided operation. For fair LP token distribution, we
-      should actualy add to all tokens in the proper ratio, like UniSwap does with the two tokens in
-      each pair. However, with up to 16 listed tokens this becomes impractical and expensive.
-      Therefore, the liquidity is added only to a single token in practice. Mathematically, the
-      DEX behaves as if liquidity were added to all listed tokens, and then all the added liquidity is
-      swapped back to the selected token. For N listed tokens, we get:
-
-            R = (1 + X_supplied/X_initial)^(1/N) - 1
-            LP_minted = R * LP_total
-
-      When adding ETH, the inputToken address to be used is the NULL address.
-      Note that for withdrawal transactions, a swapping fee is taken into account. The reason for
-      this is that it would otherwise be possible to add/withdraw liquidity rather than swapping,
-      thereby avoiding the swapping fee.
+  * Single sided liquidity add. More economic at moderate liquidity amounts.
+  * Mathematically works as adding all tokens and swapping back to 1 token at no fee.
+  *
+  *         R = (1 + X_supplied/X_initial)^(1/N) - 1
+  *         LP_minted = R * LP_total
+  *
+  * When adding ETH, the inputToken address to be used is the NULL address.
+  * A fee is applied to prevent zero fee swapping through liquidity add/remove.
   */
   function addLiquidity(address inputToken, uint256 inputAmount, uint256 minLP)
     external
@@ -152,9 +161,11 @@ contract DeFiPlaza is IDeFiPlaza, Ownable, ERC20 {
     override
     returns (uint256 actualLP)
   {
+    // Check that the exchange is unlocked and thus open for business
     Config memory _config = DFP_config;
     require(_config.unlocked, "DFP: Locked");
 
+    // Pull in input token and check the exchange balance for that token
     uint256 initialBalance;
     if (inputToken == address(0)) {
       require(msg.value == inputAmount, "DFP: Incorrect amount of ETH");
@@ -166,8 +177,11 @@ contract DeFiPlaza is IDeFiPlaza, Ownable, ERC20 {
         "DFP: Transfer failed"
       );
     }
+
+    // Prevent excessive liquidity add which runs of the approximation curve
     require(inputAmount < initialBalance, "DFP: Too much at once");
 
+    // 6th power binomial series approximation of R
     uint256 X = (inputAmount * _config.oneMinusTradingFee) / initialBalance;  // 0.64 bits
     uint256 X_ = X * X;                                // X^2   0.128 bits
     uint256 R_ = (X >> 4) - (X_ * 15 >> 73);           // R2    0.64 bits
@@ -180,13 +194,21 @@ contract DeFiPlaza is IDeFiPlaza, Ownable, ERC20 {
     X_ = X_ * X;                                       // X^6   0.192 bits
     R_ = R_ - (X_ * 2417163 >> 156);                   // R6    0.64 bits
 
+    // Calculate and mint LPs to be awarded
     actualLP = R_ * totalSupply() >> 64;
     require(actualLP > minLP, "DFP: No deal");
     _mint(msg.sender, actualLP);
-    // emitting events costs gas, but I feel it is needed to allow informed governance decisions
+
+    // Emitting liquidity add event to enable better governance decisions
     emit LiquidityAdded(msg.sender, inputToken, inputAmount, actualLP);
   }
 
+  /**
+  * Multi-token liquidity add. More economic for large amounts of liquidity.
+  * Simply takes in all 16 listed tokens in ratio and mints the LPs accordingly.
+  * For ETH, the inputToken address to be used is the NULL address.
+  * A fee is applied to prevent zero fee swapping through liquidity add/remove.
+  */
   function addMultiple(address[] calldata tokens, uint256[] calldata maxAmounts)
     external
     payable
@@ -249,24 +271,22 @@ contract DeFiPlaza is IDeFiPlaza, Ownable, ERC20 {
   }
 
   /**
-      Withdrawing liquidity from the DEX is a single sided operation. For fair LP token liquiadation
-      the liquidity should be withdrawn from all tokens in the appropriate ratios. However, with up
-      to 16 listed tokens this becomes impractical and expensive. Thus, liquidity is only withdrawn
-      from a single token instead. Mathematically, the DEX behaves as if the liquidity was indeed
-      withdrawn from all listed tokens, and then swapped back to the selected token at no fee.
-      For N listed tokens, this works out to:
-
-          R = LP_burnt / LP_initial
-          X_out = X_initial * (1 - (1 - R)^N)
-
-   */
+  * Single sided liquidity withdrawal. More efficient at lower liquidity amounts.
+  * Mathematically withdraws 16 tokens in ratio and then swaps 15 back in at no fees.
+  * Calculates the following:
+  *
+  *        R = LP_burnt / LP_initial
+  *        X_out = X_initial * (1 - (1 - R)^N)
+  *
+  * No fee is applied for withdrawals. For ETH output, use the NULL address as outputToken.
+  */
   function removeLiquidity(uint256 LPamount, address outputToken, uint256 minOutputAmount)
     external
     onlyListedToken(outputToken)
     override
     returns (uint256 actualOutput)
   {
-    // no lock check -- can remove liquidity even if exchange is locked for trading
+    // Checks the initial balance of the token desired as output token
     uint256 initialBalance;
     if (outputToken == address(0)) {
       initialBalance = address(this).balance;
@@ -274,7 +294,7 @@ contract DeFiPlaza is IDeFiPlaza, Ownable, ERC20 {
       initialBalance = IERC20(outputToken).balanceOf(address(this));
     }
 
-    // Actual amount of output token calculation.
+    // Calculates intermediate variable F = (1-R)^16 and then the resulting output amount.
     uint256 F_;
     F_ = (1 << 64) - (LPamount << 64) / totalSupply();   // (1-R)      (0.64 bits)
     F_ = F_ * F_;                                       // (1-R)^2    (0.128 bits)
@@ -284,6 +304,7 @@ contract DeFiPlaza is IDeFiPlaza, Ownable, ERC20 {
     actualOutput = initialBalance * ((1 << 64) - F_) >> 64;
     require(actualOutput > minOutputAmount, "DFP: No deal");
 
+    // Burns the LP tokens and sends the output tokens
     _burn(msg.sender, LPamount);
     if (outputToken == address(0)) {
       address payable sender = msg.sender;
@@ -291,7 +312,8 @@ contract DeFiPlaza is IDeFiPlaza, Ownable, ERC20 {
     } else {
       IERC20(outputToken).transfer(msg.sender, actualOutput);
     }
-    // emitting events costs gas, but I feel it is needed to allow informed governance decisions
+
+    // Emitting liquidity removal event to enable better governance decisions
     emit LiquidityRemoved(msg.sender, outputToken, actualOutput, LPamount);
   }
 
@@ -345,29 +367,37 @@ contract DeFiPlaza is IDeFiPlaza, Ownable, ERC20 {
     uint256 maxInputAmount,
     address outputToken
   ) external override returns (uint256 outputAmount) {
+    // Check whether the valid token is being bootstrapped
     TokenSettings memory tokenToList = listedTokens[inputToken];
     require(
       tokenToList.state == State.PreListing,
       "DFP: Wrong token"
     );
+
+    // Calculate how many tokens to actually take in (clamp at max available)
     uint256 initialInputBalance = IERC20(inputToken).balanceOf(address(this));
     uint256 availableAmount = tokenToList.listingTarget - initialInputBalance;
     uint256 actualInputAmount = maxInputAmount > availableAmount ? availableAmount : maxInputAmount;
 
+    // Actually pull the tokens in
     require(
       IERC20(inputToken).transferFrom(msg.sender, address(this), actualInputAmount),
       "DFP: token transfer failed"
     );
 
+    // Check whether the output token requested is indeed being delisted
     TokenSettings memory tokenToDelist = listedTokens[outputToken];
     require(
       tokenToDelist.state == State.Delisting,
       "DFP: Wrong token"
     );
+
+    // Check how many of the output tokens should be given out and transfer those
     uint256 initialOutputBalance = IERC20(outputToken).balanceOf(address(this));
     outputAmount = actualInputAmount.mul(initialOutputBalance).div(availableAmount);
     IERC20(outputToken).transfer(msg.sender, outputAmount);
 
+    // Emit event for better governance decisions
     emit LiquidityBootstrapped(
       msg.sender,
       inputToken,
@@ -376,6 +406,7 @@ contract DeFiPlaza is IDeFiPlaza, Ownable, ERC20 {
       outputAmount
     );
 
+    // If the input token liquidity is now at the target we complete the (de)listing
     if (actualInputAmount == availableAmount) {
       tokenToList.state = State.Listed;
       listedTokens[inputToken] = tokenToList;
@@ -386,27 +417,28 @@ contract DeFiPlaza is IDeFiPlaza, Ownable, ERC20 {
   }
 
   /**
-   * @dev Update the fee structure for the exchange
-   * @param tokenToDelist The token that is being delisted
-   * @param tokenToList The token that is coming in its place
-   * @param listingTarget The amount of tokens required for the listing to become active
+   * Initiates process to delist one token and list another.
    */
   function changeListing(
     address tokenToDelist,              // Address of token to be delisted
     address tokenToList,                // Address of token to be listed
     uint112 listingTarget               // Amount of tokens needed to activate listing
   ) external onlyListedToken(tokenToDelist) onlyOwner() {
+    // Basic validity checks. ETH cannot be delisted, only one delisting at a time.
     require(tokenToDelist != address(0), "DFP: Cannot delist ETH");
     ListingUpdate memory update = listingUpdate;
     require(update.tokenToDelist == address(0), "DFP: Previous update incomplete");
 
+    // Can't list an already listed token
     TokenSettings memory _token = listedTokens[tokenToList];
     require(_token.state == State.Unlisted, "DFP: Token already listed");
 
+    // Set the delisting/listing struct.
     update.tokenToDelist = tokenToDelist;
     update.tokenToList = tokenToList;
     listingUpdate = update;
 
+    // Configure the token states for incoming/outgoing tokens
     _token.state = State.PreListing;
     _token.listingTarget = listingTarget;
     listedTokens[tokenToList] = _token;
@@ -414,14 +446,14 @@ contract DeFiPlaza is IDeFiPlaza, Ownable, ERC20 {
   }
 
   /**
-      Sets exchange lock.
+  * Sets exchange lock, under which swap and liquidity add (but not remove) are disabled
   */
   function lockExchange() external onlyOwner() {
     DFP_config.unlocked = false;
   }
 
   /**
-      Resets exchange lock.
+  * Resets exchange lock.
   */
   function unlockExchange() external onlyOwner() {
     DFP_config.unlocked = true;
