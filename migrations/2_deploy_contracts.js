033fbd0ddfc7c4d6934345b7d412ea1fe082def3
Jazzer9F <Jazzer9F@users.noreply.github.com>
2021-06-22 16:07:11 +0200

    updated rewards timestamp

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -26,7 +26,7 @@ module.exports = function(deployer, network, accounts) {
 
       case 1:   // Network ID 1 is for main net (and forks thereof). The real deal is deployed here.
         const addresses = require("../tokens.json");
-        await deployer.deploy(DFPgov, founder, 1624356000);  // 22nd of June 2021 12:00 GMT+2
+        await deployer.deploy(DFPgov, founder, 1624550400);  // 24nd of June 2021 18:00 GMT+2
         tokens = Object.values(addresses);
         tokens.push(DFPgov.address.toLowerCase());
         await deployer.deploy(DeFiPlaza, tokens.sort(), "DeFi Plaza Main Index", "XDP1");
@@ -50,7 +50,7 @@ module.exports = function(deployer, network, accounts) {
         await deployer.deploy(TokenN);
         await deployer.deploy(TokenY);
         await deployer.deploy(TokenZ);
-        await deployer.deploy(DFPgov, accounts[5], 1624356000);
+        await deployer.deploy(DFPgov, accounts[5], 1624550400);
         tokens = [TokenA.address.toLowerCase(), TokenB.address.toLowerCase(), TokenC.address.toLowerCase(),
           TokenD.address.toLowerCase(), TokenE.address.toLowerCase(), TokenF.address.toLowerCase(),
           TokenG.address.toLowerCase(), TokenH.address.toLowerCase(), TokenI.address.toLowerCase(),
